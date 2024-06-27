159e30d95e188ddb964b1a7931408adf44c3a6fc
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-06-27 13:58:52 +0300

    eriny

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,31 +1,60 @@
+import { useState, useEffect } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { BiUser } from "react-icons/bi";
 import { RiBook2Line } from "react-icons/ri";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
+import { MdOutlineRateReview } from "react-icons/md";
+import { PiFilesLight } from "react-icons/pi";
 import { HashLink as Link } from 'react-router-hash-link';
-import { useState } from "react";
 
 import "./nav.css";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#header');
 
+  // Function to handle scroll event
+  const handleScroll = () => {
+    const sections = ['#header', '#about', '#experience', '#services', '#portfolio', '#testimonials', '#contact'];
+    const scrollPosition = window.scrollY + 200; // Adjust this value if needed
+
+    for (const section of sections) {
+      const element = document.querySelector(section);
+      if (element && element.offsetTop <= scrollPosition && element.offsetTop + element.offsetHeight > scrollPosition) {
+        setActiveNav(section);
+      }
+    }
+  };
+
+  // Add scroll event listener when component mounts
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <nav>
-      <Link to="#header"  onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}>
+      <Link to="#header" className={activeNav === '#header' ? 'active' : ''}>
         <AiOutlineHome />
       </Link>
-      <Link to="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>
+      <Link to="#about" className={activeNav === '#about' ? 'active' : ''}>
         <BiUser />
       </Link>
-      <Link to="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>
+      <Link to="#experience" className={activeNav === '#experience' ? 'active' : ''}>
         <RiBook2Line />
       </Link>
-      <Link to="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>
+      <Link to="#services" className={activeNav === '#services' ? 'active' : ''}>
         <RiServiceLine />
       </Link>
-      <Link to="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
+      <Link to="#portfolio" className={activeNav === '#portfolio' ? 'active' : ''}>
+        <PiFilesLight />
+      </Link>
+      <Link to="#testimonials" className={activeNav === '#testimonials' ? 'active' : ''}>
+        <MdOutlineRateReview />
+      </Link>
+      <Link to="#contact" className={activeNav === '#contact' ? 'active' : ''}>
         <BiMessageSquareDetail />
       </Link>
     </nav>
