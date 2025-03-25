e227ab0d73ec941662b7e5e62a639980df790f4c
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2025-03-25 15:23:52 +0200

    update profile & navbar

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,11 @@
 // import VS from "../../assets/projects/VS.png";
 // import IC from "../../assets/projects/IC.png";
 import YASEER from "../../assets/projects/Yaseer.png";
-// import MINA_HEMAIA from "../../assets/projects/mena_hemaia.png";
+import MINA_HEMAIA from "../../assets/projects/mena_hemaia.png";
 import AIRBNB_CLONE from "../../assets/projects/airbnb-clone.png";
 import SHOPPERS from "../../assets/projects/shoppers.png";
+import TAMAYOZ from "../../assets/projects/Tamayoz.png";
+import CHAT from "../../assets/projects/Chat.png";
 
 // import IMG_BG from "../../assets/projects-bg.jpg";
 
@@ -12,13 +14,14 @@ import "./portfolio.css";
 const data = [
   {
     id: 1,
-    image: SHOPPERS,
-    title: "E-commerce",
-    github: "https://github.com/ErinyAnis/e-commerce.git",
-    demo: "https://shoppers-one.vercel.app/",
+    image: TAMAYOZ,
+    title: "Tamayoz",
+    github: "https://github.com/ErinyAnis/tamayoz.git",
+    demo: "https://tamayoz.netlify.app/",
     description:
-      "Developed an e-commerce platform using Next.js, TypeScript, Tailwind CSS, and Redux Toolkit. Key features include a dynamic product catalog, advanced search, real-time shopping cart updates, and secure Stripe payments, with content management via Sanity and backend support from Firebase.",
+      "Freelance project developing a corporate website for a Saudi consultancy using TypeScript, Next.js, and Tailwind CSS to build Tamayoz with clean code and high performance.",
   },
+
   {
     id: 2,
     image: AIRBNB_CLONE,
@@ -37,16 +40,34 @@ const data = [
     description:
       "Yaseer is a leading Saudi establishment providing accounting and tax services to companies and organizations.",
   },
+  {
+    id: 4,
+    image: SHOPPERS,
+    title: "E-commerce",
+    github: "https://github.com/ErinyAnis/e-commerce.git",
+    demo: "https://shoppers-one.vercel.app/",
+    description:
+      "Developed an e-commerce platform using Next.js, TypeScript, Tailwind CSS, and Redux Toolkit. Key features include a dynamic product catalog, advanced search, real-time shopping cart updates, and secure Stripe payments, with content management via Sanity and backend support from Firebase.",
+  },
+  {
+    id: 5,
+    image: CHAT,
+    title: "Real-Time Chat Application",
+    github: "https://github.com/ErinyAnis/e-commerce.git",
+    demo: "https://shoppers-one.vercel.app/",
+    description:
+      "Built a real-time chat app using React, Tailwind CSS, Socket.io, and Axios for instant messaging. Integrated Socket.io for real-time communication, Axios for API handling, and designed a responsive UI for a seamless user experience",
+  },
+  {
+      id: 6,
+      image: MINA_HEMAIA,
+      title: "mena Hemaia",
+      github: "https://github.com/ErinyAnis/MealsApp.git",
+      demo: "https://mena-hemaia.vercel.app/",
+      description:
+        "Built a portfolio website for finance expert Mena Hemaia, highlighting his roles, qualifications, and commitment to financial education.",
+    },
 
-  // {
-  //   id: 4,
-  //   image: IC,
-  //   title: "International Contractors",
-  //   github: "https://github.com/ErinyAnis/international-contractors.git",
-  //   demo: "https://intcot.com/",
-  //   description:
-  //     "Developed a responsive, user-friendly website for IC company, leveraging 30+ years of industry expertise in Egyptian ports. Utilized HTML5, CSS3, Bootstrap 5, and jQuery for seamless navigation and interactive features.",
-  // },
   // {
   //   id: 5,
   //   image: VS,
@@ -56,15 +77,7 @@ const data = [
   //   description:
   //     "Recreated Visual Studio using React and TypeScript, showcasing interactive features and customizable UI.",
   // },
-  // {
-  //   id: 6,
-  //   image: MINA_HEMAIA,
-  //   title: "mena Hemaia",
-  //   github: "https://github.com/ErinyAnis/MealsApp.git",
-  //   demo: "https://mena-hemaia.vercel.app/",
-  //   description:
-  //     "Built a portfolio website for finance expert Mena Hemaia, highlighting his roles, qualifications, and commitment to financial education.",
-  // },
+  //
 ];
 
 const Portfolio = () => {
@@ -82,7 +95,7 @@ const Portfolio = () => {
               <div className="col-md-6 col-lg-4" key={id}>
                 <article className="portfolio__item">
                   <div className="portfolio__item-image">
-                    <img src={image} alt={title} />
+                    <img src={image} alt={title} loading="lazy" />
                   </div>
                   <h3>{title}</h3>
                   <p>{description}</p>
