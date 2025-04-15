2c21ee7c4b3c07c4079ab17beb51cbfaa0364cb4
Kerolos2000 <kero.magdy2000@gmail.com>
2025-04-16 01:09:00 +0200

    fix  base  url and add vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ErinyAnis-portfolio/', // Replace with your GitHub repository name
 });
